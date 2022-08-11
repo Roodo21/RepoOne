77b9ed70e52908a985a41210f36c5db1a4304c23
Roodo21 <rajaganesh217@gmail.com>
2022-08-11 12:15:46 +0530

    demo file commit

---
+++ src/main/java/TestApp.java
@@ -0,0 +1,9 @@
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+public class TestApp {
+    public static void main(String[] args){
+        System.out.println("Hi..");
+        System.out.println();
+    }
+}
